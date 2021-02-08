92fd0edeab7bf8bc62ed8a5fc749c63d60d39605
ishak <ijmilou@roc-dev.com>
2021-02-08 11:48:40 +0100

    klaar met opdracht 5

---
+++ H03/Opdracht5/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$leeftijd = 15;
+$prijs = 10;
+
+if ($leeftijd >65){
+    $prijs = $prijs * 0.5;
+}
+
+if ($leeftijd <=12){
+    $prijs = $prijs * 0.5;
+}
+
+if ($leeftijd <3) {
+    $prijs = 0;
+}
