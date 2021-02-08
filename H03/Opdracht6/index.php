6cb71faa13f57626c2577f844eb10633eb6b1ac6
ishak <ijmilou@roc-dev.com>
2021-02-08 12:41:00 +0100

    bezig met opdracht 5 tabel nu klaar

---
+++ H03/Opdracht6/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+
+<?php
+
+$deSpartelKuikens = 25;
+$deWaterBuffels = 32;
+$plonsmderin = 11;
+$Bommetje = 23;
+
+?>
+
+<h2>Zwemclubs</h2>
+
+<table border="1px solid black ">
+    <tr>
+        <td>
+            de spartelkuikens
+        </td>
+        <td>
+            25
+        </td>
+    </tr>
+    <tr>
+        <td>
+            de waterbuffels
+        </td>
+        <td>
+            32
+        </td>
+    </tr>
+    <tr>
+        <td>
+            plonsmderin
+        </td>
+        <td>
+            11
+        </td>
+    </tr>
+    <tr>
+        <td>
+            bommetje
+        </td>
+        <td>
+            23
+        </td>
+    </tr>
+</table>
+</body>
+</html>
