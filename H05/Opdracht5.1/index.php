1a0f054a75db26e2d8187019ac539e6431c0d1b3
ishak <ijmilou@roc-dev.com>
2021-02-16 09:28:37 +0100

    h05 opdrcht 1 klaar

---
+++ H05/Opdracht5.1/index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <title>opdr 5.1</title>
+</head>
+<body>
+<form action="resultaat.php">
+    <input type="text" name="voornaam"><br>
+    <input type="password" name="wachtwoord">
+    <input type="submit" name="knop">
+</form>
+</body>
+</html>
+
+<?php
+?>
