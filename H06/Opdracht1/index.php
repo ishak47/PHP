2df6598f1a3d7302bb0f7f040d877636956a631c
ishak <ijmilou@roc-dev.com>
2021-04-21 13:20:18 +0200

    alles weer laten werken

--- H06/Opdracht1/index.php
+++ H06/Opdracht1/index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        table, tr, td{
+            border: solid 1px black;
+            border-collapse: collapse;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+<table>
 <?php
 
 $host = 'localhost';
@@ -8,12 +23,30 @@ $db = 'school';
 
 try {
     $dbh = new PDO('mysql:host='.$host.';dbname='.$db.';port='.$port , $user,$password);
-    foreach ($dbh->query('SELECT * from cursist') as $row) {
-    print_r($row);
-    }
-    $dbh = null;
 } catch (PDOException $e){
-    print "ERROR!: " . $e->getMessage() . "<br/>";
-    die();
+    echo "ERROR!: " . $e->getMessage();
+}
+
+$query = "SELECT * from cursist";
+$stmt = $dbh->prepare($query) or die("error1");
+$stmt->execute() or die("error2");
+
+while ($row = $stmt->fetch()){
+    echo "<tr>";
+    echo "<td>" . $row['cursistnr'] . "</td>";
+    echo "<td>" . $row['naam'] . "</td>";
+    echo "<td>" . $row['roepnaam'] . "</td>";
+    echo "<td>" . $row['straat'] . "</td>";
+    echo "<td>" . $row['postcode'] . "</td>";
+    echo "<td>" . $row['plaats'] . "</td>";
+    echo "</tr>";
 }
 ?>
+</table>
+</body>
+</html>
+
+<!--$query = "SELECT * from cursist";-->
+<!--$stmt = $dbh->prepare($query) or die("error1");-->
+<!--$stmt->execute() or die("error2");-->
+
